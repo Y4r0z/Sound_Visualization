ad3340b6940357278354d093fe01de3928a19d4c
Y4r0z <72458277+Y4r0z@users.noreply.github.com>
2021-10-19 17:44:29 +0300

    Подчищен код

--- main.cpp
+++ main.cpp
@@ -16,18 +16,22 @@ float getSum(vector<float> s, int a, int b);
 
 int main()
 {
-    string fileName = "Files/bfg.ogg";
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 2;
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Audio visualizer",sf::Style::Fullscreen,settings);
-
+    string fileName = "Files/music.ogg";
     int circleRadius = 250;
     int circleResolution = 512;
     int chunkSamplesCount = 1024; //pow(2,n)
     int maxHeight = 250;
     int offset = 15;
     float holdPower = 0.9f;
-    float interpolationPower = 0.9f;
+    float interpolationPower = 0.85f;
+    float outlineSize = 1.04f;
+
+
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 2;
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Audio visualizer",sf::Style::Fullscreen,settings);
+
+    sf::Vector2f centerPos = sf::Vector2f(window.getSize().x/2,window.getSize().y/2);
 
     valarray<complex<double>> complexArray;
     complexArray.resize(chunkSamplesCount);
@@ -38,7 +42,7 @@ int main()
         leveler.push_back(0.1f + ((float)circleResolution/(float)chunkSamplesCount) * abs(sin(2 * M_PI * i / chunkSamplesCount)));
     }
 
-    sf::Vector2f centerPos = sf::Vector2f(window.getSize().x/2,window.getSize().y/2);
+
 
     vector<float> sizes;
     sizes.resize(chunkSamplesCount);
@@ -46,26 +50,16 @@ int main()
     vector<float> prevSizes;
     sizes.resize(chunkSamplesCount);
 
+
     sf::CircleShape mainCircle(circleRadius,256);
     mainCircle.setPosition(window.getSize().x/2 - circleRadius,window.getSize().y/2 - circleRadius);
+    mainCircle.setFillColor(sf::Color::Black);
 
+    sf::VertexArray shape(sf::TriangleFan, circleResolution + 2);
+    shape[0].position = centerPos;
 
-    sf::VertexArray shape3(sf::TriangleFan, circleResolution+2);
-    shape3[0].position = centerPos;
-
-    sf::VertexArray shape4(sf::TriangleFan, circleResolution+2);
-    shape4[0].position = centerPos;
-
-
-    sf::ConvexShape shape;
-    shape.setPointCount(circleResolution);
-    shape.setPosition(centerPos);
-
-    sf::ConvexShape shape2;
-    shape2.setPointCount(circleResolution);
-    shape2.setPosition(centerPos);
-
-
+    sf::VertexArray shape2(sf::TriangleFan, circleResolution + 2);
+    shape2[0].position = centerPos;
 
 
     sf::SoundBuffer buffer;
@@ -104,11 +98,11 @@ int main()
         prevSizes = sizes;
 
         int playProgress = sound.getPlayingOffset().asSeconds() * sampleRate;
-
         for(int i = playProgress; i < chunkSamplesCount + playProgress && i < sampleCount; i++)
         {
             complexArray[i-playProgress] = complex<double>(buffer.getSamples()[i],0.0);
         }
+
         fft(complexArray);
 
         for (int i = 0; i < chunkSamplesCount && i < sampleCount; i++) {
@@ -120,66 +114,42 @@ int main()
 
         }
 
-
         hold(prevSizes,sizes, holdPower);
         interpolate(sizes, interpolationPower);
 
         for(int i = 0; i < circleResolution; i++)
         {
-            sf::Vector2f posOffset = ((float)circleRadius + offset + getSum(sizes,
-                                                                i * (chunkSamplesCount / circleResolution),
-                                                                i * (chunkSamplesCount / circleResolution) + (chunkSamplesCount / circleResolution))) *
+            float sK = (float)chunkSamplesCount / (float)circleResolution;
+
+            sf::Vector2f posOffset = ((float)circleRadius + offset + getSum(sizes,i * (sK),i * (sK) + (sK))) *
                                sf::Vector2f(sinf((i) * 2 * M_PI / circleResolution),
                                             cosf((i) * 2 * M_PI / circleResolution)
                                );
-            sf::Vector2f posOffset2 = ((float)circleRadius + offset + getSum(prevSizes,
-                                                                            i * (chunkSamplesCount / circleResolution),
-                                                                            i * (chunkSamplesCount / circleResolution) + (chunkSamplesCount / circleResolution))) *
+            sf::Vector2f posOffset2 = ((float)circleRadius + offset + getSum(prevSizes,i * (sK),i * (sK) + (sK))) *
                                      sf::Vector2f(sinf((i) * 2 * M_PI / circleResolution),
                                                   cosf((i) * 2 * M_PI / circleResolution)
                                      );
 
 
-            shape3[i+1].position = centerPos + posOffset;
-            shape4[i+1].position = centerPos + posOffset2 * 1.04f;
-            /*
-            shape.setPoint(i,
-                           ((float)circleRadius + getSum(sizes,
-                                                         i * (chunkSamplesCount / circleResolution),
-                                                         i * (chunkSamplesCount / circleResolution) + (chunkSamplesCount / circleResolution))) *
-                           sf::Vector2f(sinf((i) * 2 * M_PI / circleResolution),
-                                        cosf((i) * 2 * M_PI / circleResolution)
-                           ));
-
-            shape2.setPoint(i,
-                           ((float)circleRadius + getSum(prevSizes,
-                                                         i * (chunkSamplesCount / circleResolution),
-                                                         i * (chunkSamplesCount / circleResolution) + (chunkSamplesCount / circleResolution)) + maxHeight/14) *
-                           sf::Vector2f(sinf((i) * 2 * M_PI / circleResolution),
-                                        cosf((i) * 2 * M_PI / circleResolution)
-                           ));
-            */
+            shape[i + 1].position = centerPos + posOffset;
+            shape2[i + 1].position = centerPos + posOffset2 * outlineSize;
 
         }
-        shape3[circleResolution+1].position = shape3[1].position;
-        shape4[circleResolution+1].position = shape4[4].position;
+        shape[circleResolution + 1].position = shape[1].position;
+        shape2[circleResolution + 1].position = shape2[4].position;
+
         float k = (((float)sampleSumm/(float)chunkSamplesCount)/(float)maxHeight) * 3;
         for (int i = 0; i < circleResolution+2; ++i)
         {
-            shape4[i].color = sf::Color(255 - k * 100,k * 50,k * 255);
+            shape2[i].color = sf::Color(255 - k * 100, k * 50, k * 255);
         }
 
         window.clear(sf::Color::Black);
-        /*shape2.setFillColor(sf::Color(255 - k * 100
-                ,k * 50,
-                                        k * 255));
+
         window.draw(shape2);
         window.draw(shape);
-        */
-        window.draw(shape4);
-        window.draw(shape3);
-        mainCircle.setFillColor(sf::Color::Black);
         window.draw(mainCircle);
+
         window.display();
     }
 
