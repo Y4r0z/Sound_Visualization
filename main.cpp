9d756180e4be1490dfdb9c29fcf48e301761efd4
Y4r0z <72458277+Y4r0z@users.noreply.github.com>
2021-10-19 11:43:00 +0300

    Улучшена производительность
    Улучшена производительность
    Ухудшена информативность

--- main.cpp
+++ main.cpp
@@ -16,15 +16,18 @@ float getSum(vector<float> s, int a, int b);
 
 int main()
 {
-    string fileName = "Files/ride.ogg";
+    string fileName = "Files/bfg.ogg";
     sf::ContextSettings settings;
     settings.antialiasingLevel = 2;
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Audio visualizer",sf::Style::Fullscreen,settings);
 
     int circleRadius = 250;
-    int circleResolution = 256;
-    int chunkSamplesCount = 2048; //pow(2,n)
+    int circleResolution = 512;
+    int chunkSamplesCount = 1024; //pow(2,n)
     int maxHeight = 250;
+    int offset = 15;
+    float holdPower = 0.9f;
+    float interpolationPower = 0.9f;
 
     valarray<complex<double>> complexArray;
     complexArray.resize(chunkSamplesCount);
@@ -32,7 +35,7 @@ int main()
     vector<float> leveler;
     for(int i = 0; i < chunkSamplesCount; i++)
     {
-        leveler.push_back(0.1f +  1.f * abs(sin(2 * M_PI * i / chunkSamplesCount)));
+        leveler.push_back(0.1f + ((float)circleResolution/(float)chunkSamplesCount) * abs(sin(2 * M_PI * i / chunkSamplesCount)));
     }
 
     sf::Vector2f centerPos = sf::Vector2f(window.getSize().x/2,window.getSize().y/2);
@@ -45,6 +48,13 @@ int main()
 
     sf::CircleShape mainCircle(circleRadius,256);
     mainCircle.setPosition(window.getSize().x/2 - circleRadius,window.getSize().y/2 - circleRadius);
+
+
+    sf::VertexArray shape3(sf::TriangleFan, circleResolution+2);
+    shape3[0].position = centerPos;
+
+    sf::VertexArray shape4(sf::TriangleFan, circleResolution+2);
+    shape4[0].position = centerPos;
 
 
     sf::ConvexShape shape;
@@ -111,11 +121,28 @@ int main()
         }
 
 
-        hold(prevSizes,sizes,0.9f);
-        interpolate(sizes,0.9f);
+        hold(prevSizes,sizes, holdPower);
+        interpolate(sizes, interpolationPower);
 
         for(int i = 0; i < circleResolution; i++)
         {
+            sf::Vector2f posOffset = ((float)circleRadius + offset + getSum(sizes,
+                                                                i * (chunkSamplesCount / circleResolution),
+                                                                i * (chunkSamplesCount / circleResolution) + (chunkSamplesCount / circleResolution))) *
+                               sf::Vector2f(sinf((i) * 2 * M_PI / circleResolution),
+                                            cosf((i) * 2 * M_PI / circleResolution)
+                               );
+            sf::Vector2f posOffset2 = ((float)circleRadius + offset + getSum(prevSizes,
+                                                                            i * (chunkSamplesCount / circleResolution),
+                                                                            i * (chunkSamplesCount / circleResolution) + (chunkSamplesCount / circleResolution))) *
+                                     sf::Vector2f(sinf((i) * 2 * M_PI / circleResolution),
+                                                  cosf((i) * 2 * M_PI / circleResolution)
+                                     );
+
+
+            shape3[i+1].position = centerPos + posOffset;
+            shape4[i+1].position = centerPos + posOffset2 * 1.04f;
+            /*
             shape.setPoint(i,
                            ((float)circleRadius + getSum(sizes,
                                                          i * (chunkSamplesCount / circleResolution),
@@ -127,24 +154,30 @@ int main()
             shape2.setPoint(i,
                            ((float)circleRadius + getSum(prevSizes,
                                                          i * (chunkSamplesCount / circleResolution),
-                                                         i * (chunkSamplesCount / circleResolution) + (chunkSamplesCount / circleResolution)) + maxHeight/12) *
+                                                         i * (chunkSamplesCount / circleResolution) + (chunkSamplesCount / circleResolution)) + maxHeight/14) *
                            sf::Vector2f(sinf((i) * 2 * M_PI / circleResolution),
                                         cosf((i) * 2 * M_PI / circleResolution)
                            ));
+            */
 
         }
-
-
-
-
+        shape3[circleResolution+1].position = shape3[1].position;
+        shape4[circleResolution+1].position = shape4[4].position;
+        float k = (((float)sampleSumm/(float)chunkSamplesCount)/(float)maxHeight) * 3;
+        for (int i = 0; i < circleResolution+2; ++i)
+        {
+            shape4[i].color = sf::Color(255 - k * 100,k * 50,k * 255);
+        }
 
         window.clear(sf::Color::Black);
-        float k = (((float)sampleSumm/(float)chunkSamplesCount)/(float)maxHeight) * 3;
-        shape2.setFillColor(sf::Color(255 - k * 100
+        /*shape2.setFillColor(sf::Color(255 - k * 100
                 ,k * 50,
                                         k * 255));
         window.draw(shape2);
         window.draw(shape);
+        */
+        window.draw(shape4);
+        window.draw(shape3);
         mainCircle.setFillColor(sf::Color::Black);
         window.draw(mainCircle);
         window.display();
@@ -168,15 +201,11 @@ void interpolate(vector<float> &ar, float power)
 {
     for(int i = 1; i < ar.size(); i++)
     {
-        float sizeSumm = (ar[i-1] + ar[i+1] + ar[i])/3;
-
-        ar[i]= Lerp(ar[i], sizeSumm, power) ;
+        //float sizeSumm = (ar[i-1] + ar[i+1] + ar[i])/3;
+        //ar[i]= Lerp(ar[i], sizeSumm, power) ;
         ar[i]= Lerp(ar[i],ar[i-1], power);
-
-
     }
     ar[ar.size()-1] = ar[0];
-
     for(int i = ar.size()-2; i > 0; i--)
     {
         ar[i] = Lerp(ar[i], ar[i + 1], power);
@@ -207,7 +236,7 @@ void fft(valarray<complex<double>> &x) {
     fft(odd);
 
     for (int k = 0; k < N / 2; k++) {
-        complex<double> t = std::polar(1.0, -2 * M_PI * k / N) * odd[k];
+        complex<double> t = std::polar(1.0, 2 * M_PI * k / N) * odd[k];
         x[k] = even[k] + t;
         x[k + N / 2] = even[k] - t;
     }
